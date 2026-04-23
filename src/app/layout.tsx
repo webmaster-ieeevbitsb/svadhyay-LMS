f00495e639f3cef04fcdab958d8ee7c752cf9cb0
hemanth <hemanthsaikaturi@gmail.com>
2026-04-23 20:42:30 +0530

    feat: add Vercel Analytics and implement root layout with global providers and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import ProgressBar from "@/components/ui/progress-bar";
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({
           <ProgressBar />
         </Suspense>
         {children}
+        <Analytics />
         <Toaster theme="dark" position="bottom-right" richColors />
       </body>
     </html>
